0f7c05dbfa9f85f8151176067d88daac268aa191
skip.montanaro <skip.montanaro@gmail.com>
2010-02-08 22:44:48 +0000

    r64 from hg

---
+++ trunk/_testhelper23.py
@@ -0,0 +1,23 @@
+import lockfile
+from compliancetest import _ComplianceTest
+
+class ComplianceTest(_ComplianceTest):
+    def _lock_wait_unlock(self, event1, event2):
+        """Lock from another thread.  Helper for tests."""
+        from test_lockfile import _testfile
+        l = lockfile.FileLock(_testfile())
+        l.acquire()
+        try:
+            event1.set()  # we're in,
+            event2.wait() # wait for boss's permission to leave
+        finally:
+            l.release()
+
+    def test_enter(self):
+        lock = lockfile.FileLock(_testfile())
+        lock.acquire()
+        try:
+            assert lock.is_locked()
+        finally:
+            lock.release()
+        assert not lock.is_locked()
---
+++ trunk/_testhelper25.py
@@ -0,0 +1,15 @@
+from __future__ import with_statement
+
+from compliancetest import _ComplianceTest
+
+class ComplianceTest(_ComplianceTest):
+    def _lock_wait_unlock(self, event1, event2):
+        """Lock from another thread.  Helper for tests."""
+        with lockfile.FileLock(self._testfile()):
+            event1.set()  # we're in,
+            event2.wait() # wait for boss's permission to leave
+
+    def test_enter(self):
+        with lockfile.FileLock(self._testfile()):
+            assert lock.is_locked()
+        assert not lock.is_locked()
