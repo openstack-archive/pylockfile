4ded05c1497a8a004a4243393a18e89f41c51f67
skip.montanaro <skip.montanaro@gmail.com>
2010-02-08 22:58:17 +0000

    r72 from hg

--- trunk/lockfile/lockfile.py
+++ trunk/lockfile/lockfile.py
@@ -53,16 +53,15 @@ from __future__ import division
 import sys
 import socket
 import os
-import thread
 import threading
 import time
 import errno
-import urllib
 
 # Work with PEP8 and non-PEP8 versions of threading module.
-if not hasattr(threading, "current_thread"):
+try:
+    threading.current_thread
+except AttributeError:
     threading.current_thread = threading.currentThread
-if not hasattr(threading.Thread, "get_name"):
     threading.Thread.get_name = threading.Thread.getName
 
 __all__ = ['Error', 'LockError', 'LockTimeout', 'AlreadyLocked',
@@ -164,8 +163,7 @@ class LockBase:
         self.hostname = socket.gethostname()
         self.pid = os.getpid()
         if threaded:
-            name = threading.current_thread().get_name()
-            tname = "%s-" % urllib.quote(name, safe="")
+            tname = "%s-" % threading.current_thread().get_name()
         else:
             tname = ""
         dirname = os.path.dirname(self.lock_file)
@@ -236,7 +234,7 @@ class LinkFileLock(LockBase):
         try:
             open(self.unique_name, "wb").close()
         except IOError:
-            raise LockFailed("failed to create %s" % self.unique_name)
+            raise LockFailed
 
         end_time = time.time()
         if timeout is not None and timeout > 0:
@@ -334,7 +332,7 @@ class MkdirFileLock(LockBase):
                     time.sleep(wait)
                 else:
                     # Couldn't create the lock for some other reason
-                    raise LockFailed("failed to create %s" % self.lock_file)
+                    raise LockFailed
             else:
                 open(self.unique_name, "wb").close()
                 return
