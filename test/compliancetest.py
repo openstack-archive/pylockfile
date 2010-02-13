42da14f9b52e0b14ed69163dbf3352102c4ac8cc
skip.montanaro <skip.montanaro@gmail.com>
2010-02-13 19:22:07 +0000

    Split those test methods which try both threaded and non-threaded naming schemes.  More to do.  Obviously you need to have test cases when using the non-threaded naming scheme from multiple threads.

--- test/compliancetest.py
+++ test/compliancetest.py
@@ -24,101 +24,131 @@ class ComplianceTest(object):
             os.unlink(tf)
         lockfile.LockFile = self.saved_class
 
-    def test_acquire_basic(self):
+    def _test_acquire_helper(self, tbool):
         # As simple as it gets.
-        for tbool in (True, False):
-            lock = lockfile.LockFile(self._testfile(), threaded=tbool)
-            lock.acquire()
-            assert lock.is_locked()
-            lock.release()
-            assert not lock.is_locked()
+        lock = lockfile.LockFile(self._testfile(), threaded=tbool)
+        lock.acquire()
+        assert lock.is_locked()
+        lock.release()
+        assert not lock.is_locked()
 
-    def test_acquire_no_timeout(self):
+    def test_acquire_basic_threaded(self):
+        self._test_acquire_helper(True)
+
+    def test_acquire_basic_unthreaded(self):
+        self._test_acquire_helper(False)
+
+    def _test_acquire_no_timeout_helper(self, tbool):
         # No timeout test
-        for tbool in (True, False):
-            e1, e2 = threading.Event(), threading.Event()
-            t = _in_thread(self._lock_wait_unlock, e1, e2)
-            e1.wait()         # wait for thread t to acquire lock
-            lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
-            assert lock2.is_locked()
-            assert not lock2.i_am_locking()
+        e1, e2 = threading.Event(), threading.Event()
+        t = _in_thread(self._lock_wait_unlock, e1, e2)
+        e1.wait()         # wait for thread t to acquire lock
+        lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
+        assert lock2.is_locked()
+        assert not lock2.i_am_locking()
 
-            try:
-                lock2.acquire(timeout=-1)
-            except lockfile.AlreadyLocked:
-                pass
-            else:
-                lock2.release()
-                raise AssertionError("did not raise AlreadyLocked in"
-                                     " thread %s" %
-                                     threading.current_thread().get_name())
+        try:
+            lock2.acquire(timeout=-1)
+        except lockfile.AlreadyLocked:
+            pass
+        else:
+            lock2.release()
+            raise AssertionError("did not raise AlreadyLocked in"
+                                 " thread %s" %
+                                 threading.current_thread().get_name())
 
-            e2.set()          # tell thread t to release lock
-            t.join()
+        e2.set()          # tell thread t to release lock
+        t.join()
 
-    def test_acquire_with_timeout(self):
+    def test_acquire_no_timeout_threaded(self):
+        self._test_acquire_no_timeout_helper(True)
+
+    def test_acquire_no_timeout_unthreaded(self):
+        self._test_acquire_no_timeout_helper(False)
+
+    def _test_acquire_timeout_helper(self, tbool):
         # Timeout test
-        for tbool in (True, False):
-            e1, e2 = threading.Event(), threading.Event()
-            t = _in_thread(self._lock_wait_unlock, e1, e2)
-            e1.wait()                # wait for thread t to acquire lock
-            lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
-            assert lock2.is_locked()
-            try:
-                lock2.acquire(timeout=0.1)
-            except lockfile.LockTimeout:
-                pass
-            else:
-                lock2.release()
-                raise AssertionError("did not raise LockTimeout in thread %s" %
-                                     threading.current_thread().get_name())
+        e1, e2 = threading.Event(), threading.Event()
+        t = _in_thread(self._lock_wait_unlock, e1, e2)
+        e1.wait()                # wait for thread t to acquire lock
+        lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
+        assert lock2.is_locked()
+        try:
+            lock2.acquire(timeout=0.1)
+        except lockfile.LockTimeout:
+            pass
+        else:
+            lock2.release()
+            raise AssertionError("did not raise LockTimeout in thread %s" %
+                                 threading.current_thread().get_name())
 
-            e2.set()
-            t.join()
+        e2.set()
+        t.join()
 
-    def test_release_basic(self):
-        for tbool in (True, False):
-            lock = lockfile.LockFile(self._testfile(), threaded=tbool)
-            lock.acquire()
-            assert lock.is_locked()
+    def test_acquire_timeout_threaded(self):
+        self._test_acquire_timeout_helper(True)
+
+    def test_acquire_timeout_unthreaded(self):
+        self._test_acquire_timeout_helper(False)
+
+    def _test_release_basic_helper(self, tbool):
+        lock = lockfile.LockFile(self._testfile(), threaded=tbool)
+        lock.acquire()
+        assert lock.is_locked()
+        lock.release()
+        assert not lock.is_locked()
+        assert not lock.i_am_locking()
+        try:
             lock.release()
-            assert not lock.is_locked()
-            assert not lock.i_am_locking()
-            try:
-                lock.release()
-            except lockfile.NotLocked:
-                pass
-            except lockfile.NotMyLock:
-                raise AssertionError('unexpected exception: %s' %
-                                     lockfile.NotMyLock)
-            else:
-                raise AssertionError('erroneously unlocked file')
+        except lockfile.NotLocked:
+            pass
+        except lockfile.NotMyLock:
+            raise AssertionError('unexpected exception: %s' %
+                                 lockfile.NotMyLock)
+        else:
+            raise AssertionError('erroneously unlocked file')
 
-    def test_release_threaded(self):
-        for tbool in (True, False):
-            e1, e2 = threading.Event(), threading.Event()
-            t = _in_thread(self._lock_wait_unlock, e1, e2)
-            e1.wait()
-            lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
-            assert lock2.is_locked()
-            assert not lock2.i_am_locking()
-            try:
-                lock2.release()
-            except lockfile.NotMyLock:
-                pass
-            else:
-                raise AssertionError('erroneously unlocked a file locked'
-                                     ' by another thread.')
-            e2.set()
-            t.join()
+    def test_release_basic_threaded(self):
+        self._test_release_basic_helper(True)
 
-    def test_is_locked(self):
-        for tbool in (True, False):
-            lock = lockfile.LockFile(self._testfile(), threaded=tbool)
-            lock.acquire()
-            assert lock.is_locked()
-            lock.release()
-            assert not lock.is_locked()
+    def test_release_basic_unthreaded(self):
+        self._test_release_basic_helper(False)
+
+    def _test_release_from_thread_helper(self, tbool):
+        e1, e2 = threading.Event(), threading.Event()
+        t = _in_thread(self._lock_wait_unlock, e1, e2)
+        e1.wait()
+        lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
+        assert lock2.is_locked()
+        assert not lock2.i_am_locking()
+        try:
+            lock2.release()
+        except lockfile.NotMyLock:
+            pass
+        else:
+            raise AssertionError('erroneously unlocked a file locked'
+                                 ' by another thread.')
+        e2.set()
+        t.join()
+
+    def test_release_from_thread_threaded(self):
+        self._test_release_from_thread_helper(True)
+
+    def test_release_from_thread_unthreaded(self):
+        self._test_release_from_thread_helper(False)
+
+    def _test_is_locked_helper(self, tbool):
+        lock = lockfile.LockFile(self._testfile(), threaded=tbool)
+        lock.acquire()
+        assert lock.is_locked()
+        lock.release()
+        assert not lock.is_locked()
+
+    def test_is_locked_threaded(self):
+        self._test_is_locked_helper(True)
+
+    def test_is_locked_unthreaded(self):
+        self._test_is_locked_helper(False)
 
     def test_i_am_locking(self):
         lock1 = lockfile.LockFile(self._testfile(), threaded=False)
@@ -147,21 +177,26 @@ class ComplianceTest(object):
             if lock1.i_am_locking():
                 lock1.release()
 
-    def test_break_lock(self):
-        for tbool in (True, False):
-            lock = lockfile.LockFile(self._testfile(), threaded=tbool)
-            lock.acquire()
-            assert lock.is_locked()
-            lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
-            assert lock2.is_locked()
-            lock2.break_lock()
-            assert not lock2.is_locked()
-            try:
-                lock.release()
-            except lockfile.NotLocked:
-                pass
-            else:
-                raise AssertionError('break lock failed')
+    def _test_break_lock_helper(self, tbool):
+        lock = lockfile.LockFile(self._testfile(), threaded=tbool)
+        lock.acquire()
+        assert lock.is_locked()
+        lock2 = lockfile.LockFile(self._testfile(), threaded=tbool)
+        assert lock2.is_locked()
+        lock2.break_lock()
+        assert not lock2.is_locked()
+        try:
+            lock.release()
+        except lockfile.NotLocked:
+            pass
+        else:
+            raise AssertionError('break lock failed')
+
+    def test_break_lock_threaded(self):
+        self._test_break_lock_helper(True)
+
+    def test_break_lock_unthreaded(self):
+        self._test_break_lock_helper(False)
 
     def _lock_wait_unlock(self, event1, event2):
         """Lock from another thread.  Helper for tests."""
